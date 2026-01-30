1e1961b505cec99ca0c0e81f804f46b356ed3893
Kayla Robledo <kayla.robledo@cuanschutz.edu>
2026-01-30 12:03:16 -0700

    cleaned lab values, formatted time, calculate diff in time

--- Project 0/Code/01_Clean_Data.R
+++ Project 0/Code/01_Clean_Data.R
@@ -1,8 +1,5 @@
 ## Loading packages
 library(tidyverse)
-library(gtsummary)
-library(ggplot2)
-library(nlme)
 
 ## Reading in the data
 dat_p0_raw <- read.csv("DataRaw/Project0_Clean_v2.csv")
@@ -22,3 +19,37 @@ dat_p0 <- dat_p0 %>%
     )
   ) %>%
   ungroup()
+
+## Formatting as times (used ChatGPT to help with first step)
+dat_p0$Sleep.Diary.reported.wake.time <- as_hms(parse_date_time(dat_p0$Sleep.Diary.reported.wake.time, orders = "H:M"))
+dat_p0$Booket..Clock.Time <- as_hms(parse_date_time(dat_p0$Booket..Clock.Time, orders = "H:M"))
+dat_p0$MEMs..Clock.Time <- as_hms(parse_date_time(dat_p0$MEMs..Clock.Time, orders = "H:M"))
+dat_p0$calc_book_int <- as.numeric(dat_p0$Booket..Clock.Time - dat_p0$Sleep.Diary.reported.wake.time, unit = "mins")
+dat_p0$calc_mems_int <- as.numeric(dat_p0$MEMs..Clock.Time - dat_p0$Sleep.Diary.reported.wake.time, unit = "mins")
+
+## RANGES FOR LABS PER PI
+## Use nmol/L
+## Cortisol: anything over 26 is high, but exclude over 80
+## DHEA: upper detection limit of 5.205 and exclude them and let her know who
+
+## Identifying subjects with upper detection for DHEA to notify PI
+dat_p0 %>%
+  filter(DHEA..nmol.L. == 5.205) %>%
+  pull(SubjectID) %>%
+  unique()
+
+## Excluding cortisol over 80 and DHEA of 5.205
+dat_p0 <- dat_p0 %>%
+  filter(Cortisol..nmol.L. < 80 & DHEA..nmol.L. < 5.205)
+
+## Calculating number of observations excluded
+nrow(dat_p0_raw) - nrow(dat_p0)
+
+## Identifying subjects with cortisol over 26
+dat_p0 %>%
+  filter(Cortisol..nmol.L. > 26) %>%
+  pull(SubjectID) %>%
+  unique()
+
+## Saving new dataset
+write.csv(dat_p0, "DataProcessed/dat_p0_clean_kr.csv")
