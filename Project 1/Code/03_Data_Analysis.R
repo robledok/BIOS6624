a34b14843765499ee80dacc33aee755287f8819a
Kayla Robledo <kayla.robledo@cuanschutz.edu>
2026-02-27 11:03:36 -0700

    fitting bayesian models using brms with non informative priors

--- Project 1/Code/03_Data_Analysis.R
+++ Project 1/Code/03_Data_Analysis.R
@@ -104,3 +104,106 @@ AIC(freq_mod1_mqol, freq_mod2_mqol)
 summary(freq_mod1_pqol)$coefficients
 summary(freq_mod2_pqol)$coefficients
 AIC(freq_mod1_pqol, freq_mod2_pqol)
+
+##*******************************************************************
+## ------------------ Bayesian Models  ----------------------
+##*******************************************************************
+##
+
+## CD4 Models
+# Model with adherence
+bayes_mod1_cd4 <- brm(LEU3N ~ drugs_base + cd4_base + age_base + 
+                      bmi_base + smoke_binary + educ_binary + race_binary + adh_binary,
+                      data = dat_p1,
+                      seed = 6624,
+                      prior = c(set_prior("normal(0, 10000)", class = "b"),
+                              set_prior("normal(1,10000)", class = "Intercept"),
+                              set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                      chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+# Model without adherence
+bayes_mod2_cd4 <- brm(LEU3N ~ drugs_base + cd4_base + age_base + 
+                      bmi_base + smoke_binary + educ_binary + race_binary,
+                      data = dat_p1,
+                      seed = 6624,
+                      prior = c(set_prior("normal(0, 10000)", class = "b"),
+                                set_prior("normal(1,10000)", class = "Intercept"),
+                                set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                      chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+
+## Viral Load Models
+# Model with adherence
+bayes_mod1_vl <- brm(log10(VLOAD) ~ drugs_base + vl_base + age_base + 
+                     bmi_base + smoke_binary + educ_binary + race_binary + adh_binary,
+                     data = dat_p1,
+                     seed = 6624,
+                     prior = c(set_prior("normal(0, 10000)", class = "b"),
+                               set_prior("normal(1,10000)", class = "Intercept"),
+                               set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                     chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+# Model without adherence
+bayes_mod2_vl <- brm(log10(VLOAD) ~ drugs_base + vl_base + age_base + 
+                     bmi_base + smoke_binary + educ_binary + race_binary,
+                     data = dat_p1,
+                     seed = 6624,
+                     prior = c(set_prior("normal(0, 10000)", class = "b"),
+                               set_prior("normal(1,10000)", class = "Intercept"),
+                               set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                     chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+
+## Mental Quality of Life Models
+# Model with adherence
+bayes_mod1_mqol <- brm(AGG_MENT ~ drugs_base + mqol_base + age_base + 
+                       bmi_base + smoke_binary + educ_binary + race_binary + adh_binary,
+                       data = dat_p1,
+                       seed = 6624,
+                       prior = c(set_prior("normal(0, 10000)", class = "b"),
+                                 set_prior("normal(1,10000)", class = "Intercept"),
+                                 set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                       chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+# Model without adherence
+bayes_mod2_mqol <- brm(AGG_MENT ~ drugs_base + mqol_base + age_base + 
+                       bmi_base + smoke_binary + educ_binary + race_binary,
+                       data = dat_p1,
+                       seed = 6624,
+                       prior = c(set_prior("normal(0, 10000)", class = "b"),
+                                 set_prior("normal(1,10000)", class = "Intercept"),
+                                 set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                       chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+
+## Physical Quality of Life Models
+# Model with adherence
+bayes_mod1_pqol <- brm(AGG_PHYS ~ drugs_base + pqol_base + age_base + 
+                       bmi_base + smoke_binary + educ_binary + race_binary + adh_binary,
+                       data = dat_p1,
+                       seed = 6624,
+                       prior = c(set_prior("normal(0, 10000)", class = "b"),
+                                 set_prior("normal(1,10000)", class = "Intercept"),
+                                 set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                       chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+# Model without adherence
+bayes_mod2_pqol <- brm(AGG_PHYS ~ drugs_base + pqol_base + age_base + 
+                       bmi_base + smoke_binary + educ_binary + race_binary,
+                       data = dat_p1,
+                       seed = 6624,
+                       prior = c(set_prior("normal(0, 10000)", class = "b"),
+                                 set_prior("normal(1,10000)", class = "Intercept"),
+                                 set_prior("normal(0,10000)", class = "sigma"), lb = 0),
+                       chains = 4, iter = 5000, warmup = 25000, refresh = 0)
+
+## Looking at summary coefficient output
+
+# CD4 Models
+summary(bayes_mod1_cd4)$coefficients
+summary(bayes_mod2_cd4)$coefficients
+
+# Viral Load Models
+summary(bayes_mod1_vl)$coefficients
+summary(bayes_mod2_vl)$coefficients
+
+# Mental Quality of Life Models
+summary(bayes_mod1_mqol)$coefficients
+summary(bayes_mod2_mqol)$coefficients
+
+# Physical Quality of Life Models
+summary(bayes_mod1_pqol)$coefficients
+summary(bayes_mod2_pqol)$coefficients
